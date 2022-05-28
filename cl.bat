a9b0a724f87de9d988bb9c15377d971a4d923415
Lily <aerindev@protonmail.com>
2022-05-28 21:23:04 +0200

    fix CI5t45436

--- cl.bat
+++ cl.bat
@@ -1,4 +1,3 @@
-rem Compile Debug
 cl /w /MP -Zi /DEBUG:FULL /FeSkapande.exe .\source\main.c /I .\third_party\include\ ^
 /EHsc /link /SUBSYSTEM:CONSOLE /NODEFAULTLIB:msvcrt.lib /NODEFAULTLIB:LIBCMT ^
 %os_libs%
